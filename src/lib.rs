84137af1acbc87f6d6adf13a1557c8065193e002
Allan Calix <contact@allancalix.com>
2018-09-17 17:56:25 -0700

    Map hexcode bit values to base64

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@ pub enum FromHexError {
 
 impl FromHex for Vec<u8> {
     type Error = FromHexError;
-    
+
     fn from_hex<T: AsRef<[u8]>>(s: T) -> Result<Self, Self::Error> {
         let bytes = s.as_ref();
         let mut b = Vec::with_capacity(bytes.len() / 2);
@@ -51,9 +51,65 @@ impl FromHex for Vec<u8> {
     }
 }
 
+#[derive(Debug, Clone, PartialEq)]
+pub struct Base64 (Vec<u8>);
+
+impl From<Vec<u8>> for Base64 {
+    fn from(v: Vec<u8>) -> Self {
+        Base64(v.clone())
+    }
+}
+
+fn map_u8_to_base64(x: u8) -> char {
+    match x {
+        0...25 => (x + 65) as char,
+        26...51 => ((x - 26) + 97) as char,
+        52...61 => ((x - 52) + 48) as char,
+        62 => '+',
+        63 => '/',
+        _ => '=',
+    }
+}
+
+impl ToString for Base64 {
+    fn to_string(&self) -> String {
+        let mut result = String::new();
+
+        for (i, val) in self.0.iter().enumerate() {
+            match i % 3 {
+                0 => {
+                    result.push(map_u8_to_base64(val >> 2));
+                    if i == self.0.len() - 1 {
+                        result.push(map_u8_to_base64(val << 6 >> 2));
+                        result.push(map_u8_to_base64(255));
+                        result.push(map_u8_to_base64(255));
+                    }
+                }
+                1 => {
+                    let head = self.0[i - 1] << 6;
+                    let tail = val >> 2;
+                    result.push(map_u8_to_base64((head | tail) >> 2));
+
+                    if i == self.0.len() - 1 {
+                        result.push(map_u8_to_base64(val << 4 >> 2));
+                        result.push(map_u8_to_base64(255));
+                    } else {
+                        let bhead = val << 4;
+                        let btail = self.0[i + 1] >> 4;
+                        result.push(map_u8_to_base64((bhead | btail) >> 2));
+                    }
+                },
+                2 => result.push(map_u8_to_base64(val << 2 >> 2)),
+                _ => println!("oh no"),
+            }
+        }
+        result
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{FromHex, FromHexError};
+    use super::{FromHex, FromHexError, Base64};
 
     #[test]
     fn test_decode_hex_to_bits() {
@@ -75,5 +131,33 @@ mod tests {
                 index: 4,
             },
         );
+    }
+
+    #[test]
+    fn test_hex_buffer_to_base64() {
+        let buf = Vec::from_hex("68656C6C6F6D").unwrap();
+        let test = Base64::from(buf);
+        assert_eq!(test.to_string(), "aGVsbG9t".to_string());
+    }
+
+    #[test]
+    fn test_padding_once_hex_buffer_to_base64() {
+        let buf = Vec::from_hex("68656C6C6F6DA3D2").unwrap();
+        let test = Base64::from(buf);
+        assert_eq!(test.to_string(), "aGVsbG9to9I=".to_string());
+    }
+
+    #[test]
+    fn test_padded_twice_hex_buffer_to_base64() {
+        let buf = Vec::from_hex("68656C6C6F6DA3").unwrap();
+        let test = Base64::from(buf);
+        assert_eq!(test.to_string(), "aGVsbG9tow==".to_string());
+    }
+
+    #[test]
+    fn test_cryptopals() {
+        let buf = Vec::from_hex("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d").unwrap();
+        let test = Base64::from(buf);
+        assert_eq!(test.to_string(), "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t".to_string());
     }
 }
